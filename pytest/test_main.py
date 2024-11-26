77377fd92c619192c4f9fae63d8c7aa152a98b86
Sergey-Bedin <bedin2418@gmail.com>
2024-11-26 19:41:47 +0300

    first commit

---
+++ pytest/test_main.py
@@ -0,0 +1,23 @@
+from src.main import Calculator
+
+
+def test_add():
+    a = 3
+    b = 2
+    res = 5
+    assert Calculator.add(a, b) == res
+def test_sub():
+    a = 3
+    b = 2
+    res = 1
+    assert Calculator.sub(a, b) == res
+def test_multi():
+    a = 3
+    b = 2
+    res = 6
+    assert Calculator.multi(a, b) == res
+def test_div():
+    a = 3
+    b = 2
+    res = 1.5
+    assert Calculator.div(a, b) == res
