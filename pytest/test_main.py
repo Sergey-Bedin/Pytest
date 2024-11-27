4ffdfdd98850a8aab2b46a1936ac383715abf384
Sergey-Bedin <bedin2418@gmail.com>
2024-11-27 23:30:44 +0300

    3 commit

--- pytest/test_main.py
+++ pytest/test_main.py
@@ -1,46 +1,58 @@
 import pytest
+from contextlib import nullcontext as does_not_raise
+
 from src.main import Calculator
 
 class Test_Calculator:
 
     @pytest.mark.parametrize(
-            "a, b, res",
+            "a, b, res, expectation",
             [
-                (3, 2, 5),
-                (5, -3, 2)
+                (3, 2.0, 5, does_not_raise()),
+                (5, -3, 2, does_not_raise()),
+                (5, "0", 5, pytest.raises(TypeError)),
             ]
     )
-    def test_add(self, a, b, res):
-        assert Calculator().add(a, b) == res
+    def test_add(self, a, b, res, expectation):
+        with expectation:
+            assert Calculator().add(a, b) == res
 
 
     @pytest.mark.parametrize(
-                "a, b, res",
-                [
-                    (3, 2, 1),
-                    (5, -3, 8)
-                ]
+            "a, b, res, expectation",
+            [
+                (3, 2.0, 1, does_not_raise()),
+                (5, -3, 8, does_not_raise()),
+                (5, "0", 5, pytest.raises(TypeError)),
+            ]
         )
-    def test_sub(self, a, b, res):
-        assert Calculator().sub(a, b) == res
+    def test_sub(self, a, b, res, expectation):
+        with expectation:
+            assert Calculator().sub(a, b) == res
     
 
     @pytest.mark.parametrize(
-            "a, b, res",
+            "a, b, res, expectation",
             [
-                (3, 2, 6),
-                (5, -3, -15)
+                (3, 2.0, 6, does_not_raise()),
+                (5, -3, -15, does_not_raise()),
+                (4, "4", 5, pytest.raises(TypeError)),
             ]
-    )
-    def test_multi(self, a, b, res):
-        assert Calculator().multi(a, b) == res
+        )
+    def test_multi(self, a, b, res, expectation):
+        with expectation:
+            assert Calculator().multi(a, b) == res
     
+
     @pytest.mark.parametrize(
-            "a, b, res",
+            "a, b, res, expectation",
             [
-                (3, 2, 1.5),
-                (6, -3, -2)
+                (3, 2, 1.5, does_not_raise()),
+                (6, -3, -2, does_not_raise()),
+                (5, "1", 5, pytest.raises(TypeError)),
+                (5, 0, 0, pytest.raises(ZeroDivisionError)),
             ]
     )
-    def test_div(self, a, b, res):
-        assert Calculator().div(a, b) == res
+    def test_div(self, a, b, res, expectation):
+        with expectation:
+            assert Calculator().div(a, b) == res
--- src/main.py
+++ src/main.py
@@ -1,12 +1,22 @@
 class Calculator:
     def add(self, a, b):
+        if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+            raise TypeError("Both arguments should be numeric")
         return a + b
 
     def sub(self, a, b):
+        if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+            raise TypeError("Both arguments should be numeric")
         return a - b
 
     def multi(self, a, b):
+        if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+            raise TypeError("Both arguments should be numeric")
         return a * b
 
     def div(self, a, b):
+        if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+            raise TypeError("Both arguments should be numeric")
+        if b == 0:
+            raise ZeroDivisionError("Cannot divide by Zero")
         return a / b
